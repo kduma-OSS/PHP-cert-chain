94d80beb7de9171f2e33c91a4c873d34876e43bc
Krystian Duma <kduma@users.noreply.github.com>
2025-09-12 17:54:53 +0200

    BinaryString

---
+++ src/Utils/BinaryString.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace KDuma\CertificateChainOfTrust\Utils;
+
+readonly class BinaryString
+{
+    public function __construct(public string $value)
+    {
+    }
+
+    public function toString(): string
+    {
+        return $this->value;
+    }
+
+    public function toHex(): string
+    {
+        return bin2hex($this->value);
+    }
+
+    public function toBase64(): string
+    {
+        return base64_encode($this->value);
+    }
+
+    public function size(): int
+    {
+        return strlen($this->value);
+    }
+
+    public static function fromString(string $value): self
+    {
+        return new self($value);
+    }
+
+    public static function fromHex(string $hex): static
+    {
+        return new static(hex2bin($hex));
+    }
+
+    public static function fromBase64(string $base64): static
+    {
+        return new static(base64_decode($base64, true));
+    }
+
+    public function equals(BinaryString $other): bool
+    {
+        return hash_equals($this->value, $other->value);
+    }
+}
---
+++ tests/Utils/BinaryStringTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace KDuma\CertificateChainOfTrust\Tests\Utils;
+
+use KDuma\CertificateChainOfTrust\Crypto\KeyId;
+use KDuma\CertificateChainOfTrust\Crypto\PublicKey;
+use KDuma\CertificateChainOfTrust\Utils\BinaryString;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(BinaryString::class)]
+class BinaryStringTest extends TestCase
+{
+    private BinaryString $binaryString;
+
+    protected function setUp(): void
+    {
+        $this->binaryString = new BinaryString("\x01\x02\x03\x04");
+
+        parent::setUp();
+    }
+
+    public function testToHex()
+    {
+        $this->assertEquals("01020304", $this->binaryString->toHex());
+    }
+
+    public function testToBase64()
+    {
+        $this->assertEquals("AQIDBA==", $this->binaryString->toBase64());
+    }
+
+    public function testFromBase64()
+    {
+        $reconstructedString = BinaryString::fromBase64("AQIDBA==");
+
+        $this->assertEquals($this->binaryString, $reconstructedString);
+    }
+
+    public function testSize()
+    {
+        $this->assertEquals(4, $this->binaryString->size());
+    }
+
+    public function testFromHex()
+    {
+        $reconstructedString = BinaryString::fromHex("01020304");
+
+        $this->assertEquals($this->binaryString, $reconstructedString);
+    }
+
+    public function testToString()
+    {
+        $this->assertEquals("\x01\x02\x03\x04", $this->binaryString->toString());
+    }
+
+    public function testFromString()
+    {
+        $reconstructedString = BinaryString::fromString("\x01\x02\x03\x04");
+
+        $this->assertEquals($this->binaryString, $reconstructedString);
+    }
+
+    public function testEquals()
+    {
+        $this->assertTrue($this->binaryString->equals(new BinaryString("\x01\x02\x03\x04")));
+        $this->assertFalse($this->binaryString->equals(new BinaryString("\xFF\xFF\xFF\xFF")));
+    }
+}
