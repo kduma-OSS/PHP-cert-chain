604dee2465d63bedc45f975773ab21c92ae10824
Krystian Duma <kduma@users.noreply.github.com>
2025-09-12 20:43:23 +0200

    CertificateFlag

---
+++ src/Enums/CertificateFlag.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace KDuma\CertificateChainOfTrust\Enums;
+
+enum CertificateFlag: int
+{
+    case ROOT_CA = 0x0001;
+    case INTERMEDIATE_CA = 0x0002;
+    case CA = 0x0004;
+    case DOCUMENT_SIGNER = 0x0100;
+    case TEMPLATE_SIGNER = 0x0200;
+
+    public function toString(): string
+    {
+        return match ($this) {
+            self::ROOT_CA => 'Root CA',
+            self::INTERMEDIATE_CA => 'Intermediate CA',
+            self::CA => 'CA',
+            self::DOCUMENT_SIGNER => 'Document Signer',
+            self::TEMPLATE_SIGNER => 'Template Signer',
+        };
+    }
+
+    public static function fromByte(int $byte): self
+    {
+        return self::tryFrom($byte) ?? throw new \InvalidArgumentException("Invalid flag: 0x" . dechex($byte));
+    }
+}
---
+++ tests/Enums/CertificateFlagTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace KDuma\CertificateChainOfTrust\Tests\Enums;
+
+use KDuma\CertificateChainOfTrust\Enums\CertificateFlag;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(CertificateFlag::class)]
+class CertificateFlagTest extends TestCase
+{
+
+    public function testToString()
+    {
+        $this->assertEquals('Root CA', CertificateFlag::ROOT_CA->toString());
+        $this->assertEquals('Intermediate CA', CertificateFlag::INTERMEDIATE_CA->toString());
+        $this->assertEquals('CA', CertificateFlag::CA->toString());
+        $this->assertEquals('Document Signer', CertificateFlag::DOCUMENT_SIGNER->toString());
+        $this->assertEquals('Template Signer', CertificateFlag::TEMPLATE_SIGNER->toString());
+    }
+
+    public function testFromByte()
+    {
+        $this->assertEquals(CertificateFlag::ROOT_CA, CertificateFlag::fromByte(0x0001));
+        $this->assertEquals(CertificateFlag::INTERMEDIATE_CA, CertificateFlag::fromByte(0x0002));
+        $this->assertEquals(CertificateFlag::CA, CertificateFlag::fromByte(0x0004));
+        $this->assertEquals(CertificateFlag::DOCUMENT_SIGNER, CertificateFlag::fromByte(0x0100));
+        $this->assertEquals(CertificateFlag::TEMPLATE_SIGNER, CertificateFlag::fromByte(0x0200));
+
+        try {
+            CertificateFlag::fromByte(0xFFFF);
+        } catch (\InvalidArgumentException $e) {
+            $this->assertEquals("Invalid flag: 0xffff", $e->getMessage());
+        }
+    }
+}
