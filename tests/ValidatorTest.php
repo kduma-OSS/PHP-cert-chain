68706f3de4b960c8dbd2d593ef7086f42479a373
Krystian Duma <kduma@users.noreply.github.com>
2025-09-13 02:12:03 +0200

    Refactor

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -4,6 +4,12 @@ namespace KDuma\CertificateChainOfTrust\Tests;
 
 use KDuma\CertificateChainOfTrust\Certificate;
 use KDuma\CertificateChainOfTrust\Chain;
+use KDuma\CertificateChainOfTrust\Crypto\Ed25519;
+use KDuma\CertificateChainOfTrust\DTO\CertificateFlag;
+use KDuma\CertificateChainOfTrust\DTO\CertificateFlagsCollection;
+use KDuma\CertificateChainOfTrust\DTO\DescriptorType;
+use KDuma\CertificateChainOfTrust\DTO\Signature;
+use KDuma\CertificateChainOfTrust\DTO\UserDescriptor;
 use KDuma\CertificateChainOfTrust\DTO\ValidationError;
 use KDuma\CertificateChainOfTrust\DTO\ValidationResult;
 use KDuma\CertificateChainOfTrust\DTO\ValidationWarning;
@@ -16,26 +22,59 @@ use PHPUnit\Framework\TestCase;
 #[CoversClass(Validator::class)]
 class ValidatorTest extends TestCase
 {
-    const array EXAMPLE_CERTS = [
-        'CERTAcvqzcCWF+Plt90g93ZAGV6WOt6GDZe80h4KaD5cocEA2E/oMEJQomXDGR14OzSljRpFeGFtcGxlIFNpZ25lciBDZXJ0aWZpY2F0ZQEDABJzaWduZXIuZXhhbXBsZS5jb20DAAEeAgBwvi0pC50NSVuou6TUc8wOL6hEIBn4DdZJtnRTG00H1B0UJZRKAWWo2YOYYW/f4gJsBaQ3NGC2G45xXg5B++gpEhowfZNY8EFV1zqLBQ==',
-        'CERTAR4CAHC+LSkLnQ1JW6i7pNTOJOAFvUe856zNgY4Dm0uxCPKHR0zDc58vziPsAU7JghZFeGFtcGxlIENBIENlcnRpZmljYXRlAQMADmNhLmV4YW1wbGUuY29tAwQB41bjk0DLeETOIgPuW8Kx9ow9xhT+ylstRfvIktC9EO28uT/Z4lTAa+H52d1t7qZ5Qc5GXD6Q6l5rS9ASgQgZTgDsyfBlKUUFxzC9tvUADww=',
-        'CERTAeNW45NAy3hEziID7lvCsfZ3EMHnQFCvqUxr07jGQXyl8wQ83TpNGnOCEGxOUYg1CCNFeGFtcGxlIEludGVybWVkaWF0ZSBDQSBDZXJ0aWZpY2F0ZQEDABhpbnRlcm1lZGlhdGUuZXhhbXBsZS5jb20DBgFY0TIpXLtX/tk79S+G+oCdPB8ECk4b15eCMYPFAuBxipqF2Nwjj847RvLaw08DPHu7/7Uh7U1QdfntbO5sJLbw2bXx6d5PaaGKpGOnqOrGBQ==',
-        'CERTAVjRMilcu1f+2Tv1L4b6gJ3HVeE/YCex4hSFvT1RjNnhtRZovhI8NjSj2rUiiOiTCxtFeGFtcGxlIFJvb3QgQ0EgQ2VydGlmaWNhdGUBAwAQcm9vdC5leGFtcGxlLmNvbQMHAVjRMilcu1f+2Tv1L4b6gJ0eZuMAzrTmI10gs8GZjruuy5tlczYYcRnV9UN4r+fCo05FEqmui2/WJVVBi2Nj+H3DaMrlRC62so24CSta31AH',
-    ];
+
+    protected array $certificates = [];
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->certificates = [];
+    }
+
+    protected function makeTestCert(string $name, array $flags, array $signed_by): Certificate {
+        $this->certificates[$name] = [
+            'key' => Ed25519::makeKeyPair(),
+            'certificate' => null,
+        ];
+
+        $this->certificates[$name]['certificate'] = new Certificate(
+            key: $this->certificates[$name]['key']->toPublicKey(),
+            description: $name,
+            userDescriptors: [
+                new UserDescriptor(DescriptorType::USERNAME, $name),
+            ],
+            flags: CertificateFlagsCollection::fromList($flags),
+            signatures: []
+        );
+        $signatures = [];
+        foreach ($signed_by as $signerName) {
+            if (!isset($this->certificates[$signerName])) {
+                throw new \InvalidArgumentException("Signer certificate '$signerName' not found for '$name'");
+            }
+            $signatures[] = Signature::make($this->certificates[$name]['certificate']->toBinaryForSigning(), $this->certificates[$signerName]['key']);
+        }
+        $this->certificates[$name]['certificate'] = $this->certificates[$name]['certificate']->with(signatures:$signatures);
+
+        return $this->certificates[$name]['certificate'];
+    }
 
     public function testValidateCompleteValidChain()
     {
-        // Create complete certificate chain
+        // Create complete certificate chain using makeTestCert
+        $root_ca = $this->makeTestCert('root_ca', [CertificateFlag::ROOT_CA, CertificateFlag::DOCUMENT_SIGNER], ['root_ca']);
+        $intermediate_ca = $this->makeTestCert('intermediate_ca', [CertificateFlag::INTERMEDIATE_CA, CertificateFlag::DOCUMENT_SIGNER], ['root_ca']);
+        $ca = $this->makeTestCert('ca', [CertificateFlag::CA, CertificateFlag::DOCUMENT_SIGNER], ['intermediate_ca']);
+        $signer = $this->makeTestCert('signer', [CertificateFlag::DOCUMENT_SIGNER], ['ca']);
+
         $chain = new Chain([
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[0])), // Signer
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[1])), // CA
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[2])), // Intermediate CA
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[3])), // Root CA
+            $signer,
+            $ca,
+            $intermediate_ca,
+            $root_ca,
         ]);
 
         // Create trust store with root CA
         $trustStore = new TrustStore([
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[3])), // Root CA
+            $root_ca,
         ]);
 
         // Validate the chain using static method
@@ -49,17 +88,19 @@ class ValidatorTest extends TestCase
         $this->assertCount(4, $result->validatedChain);
 
         // Verify validated chain contains correct certificates in correct order
-        $this->assertEquals('cbeacdc09617e3e5b7dd20f77640195e', $result->validatedChain[0]->key->id->toHex());
-        $this->assertEquals('1e020070be2d290b9d0d495ba8bba4d4', $result->validatedChain[1]->key->id->toHex());
-        $this->assertEquals('e356e39340cb7844ce2203ee5bc2b1f6', $result->validatedChain[2]->key->id->toHex());
-        $this->assertEquals('58d132295cbb57fed93bf52f86fa809d', $result->validatedChain[3]->key->id->toHex());
+        $this->assertEquals($signer->key->id->toHex(), $result->validatedChain[0]->key->id->toHex());
+        $this->assertEquals($ca->key->id->toHex(), $result->validatedChain[1]->key->id->toHex());
+        $this->assertEquals($intermediate_ca->key->id->toHex(), $result->validatedChain[2]->key->id->toHex());
+        $this->assertEquals($root_ca->key->id->toHex(), $result->validatedChain[3]->key->id->toHex());
     }
 
     public function testValidateEmptyChain()
     {
+        $root_ca = $this->makeTestCert('root_ca', [CertificateFlag::ROOT_CA, CertificateFlag::DOCUMENT_SIGNER], ['root_ca']);
+        
         $emptyChain = new Chain([]);
         $trustStore = new TrustStore([
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[3])),
+            $root_ca,
         ]);
 
         $result = Validator::validateChain($emptyChain, $trustStore);
@@ -76,15 +117,20 @@ class ValidatorTest extends TestCase
 
     public function testValidateIncompleteChain()
     {
-        // Create incomplete chain (missing intermediate and root CA)
+        // Create certificates but use incomplete chain (missing intermediate and root CA)
+        $root_ca = $this->makeTestCert('root_ca', [CertificateFlag::ROOT_CA, CertificateFlag::DOCUMENT_SIGNER], ['root_ca']);
+        $intermediate_ca = $this->makeTestCert('intermediate_ca', [CertificateFlag::INTERMEDIATE_CA, CertificateFlag::DOCUMENT_SIGNER], ['root_ca']);
+        $ca = $this->makeTestCert('ca', [CertificateFlag::CA, CertificateFlag::DOCUMENT_SIGNER], ['intermediate_ca']);
+        $signer = $this->makeTestCert('signer', [CertificateFlag::DOCUMENT_SIGNER], ['ca']);
+        
         $incompleteChain = new Chain([
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[0])), // Signer
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[1])), // CA
+            $signer,
+            $ca,
             // Missing intermediate CA and root CA
         ]);
 
         $trustStore = new TrustStore([
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[3])), // Root CA
+            $root_ca,
         ]);
 
         $result = Validator::validateChain($incompleteChain, $trustStore);
@@ -97,18 +143,23 @@ class ValidatorTest extends TestCase
 
         $errorMessages = $result->getErrorMessages();
         $this->assertStringContainsString('No complete certification path found', $errorMessages[0]);
-        $this->assertStringContainsString('Certificate: cbeacdc09617e3e5b7dd20f77640195e', $errorMessages[0]);
+        $this->assertStringContainsString('Certificate: ' . $signer->key->id->toHex(), $errorMessages[0]);
         $this->assertStringContainsString('[path building]', $errorMessages[0]);
     }
 
     public function testValidateRootNotInTrustStore()
     {
         // Create complete valid chain
+        $root_ca = $this->makeTestCert('root_ca', [CertificateFlag::ROOT_CA, CertificateFlag::DOCUMENT_SIGNER], ['root_ca']);
+        $intermediate_ca = $this->makeTestCert('intermediate_ca', [CertificateFlag::INTERMEDIATE_CA, CertificateFlag::DOCUMENT_SIGNER], ['root_ca']);
+        $ca = $this->makeTestCert('ca', [CertificateFlag::CA, CertificateFlag::DOCUMENT_SIGNER], ['intermediate_ca']);
+        $signer = $this->makeTestCert('signer', [CertificateFlag::DOCUMENT_SIGNER], ['ca']);
+        
         $chain = new Chain([
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[0])),
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[1])),
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[2])),
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[3])),
+            $signer,
+            $ca,
+            $intermediate_ca,
+            $root_ca,
         ]);
 
         // Create empty trust store (root CA not trusted)
@@ -125,22 +176,27 @@ class ValidatorTest extends TestCase
         $errorMessages = $result->getErrorMessages();
         $this->assertStringContainsString('No valid certification path found', $errorMessages[0]);
         $this->assertStringContainsString('Root CA is not in the trust store', $errorMessages[1]);
-        $this->assertStringContainsString('Certificate: 58d132295cbb57fed93bf52f86fa809d', $errorMessages[1]);
+        $this->assertStringContainsString('Certificate: ' . $root_ca->key->id->toHex(), $errorMessages[1]);
         $this->assertStringContainsString('[trust store validation]', $errorMessages[1]);
     }
 
     public function testValidationResultStructure()
     {
-        // Test with valid chain
+        // Test with valid chain using makeTestCert
+        $root_ca = $this->makeTestCert('root_ca', [CertificateFlag::ROOT_CA, CertificateFlag::DOCUMENT_SIGNER], ['root_ca']);
+        $intermediate_ca = $this->makeTestCert('intermediate_ca', [CertificateFlag::INTERMEDIATE_CA, CertificateFlag::DOCUMENT_SIGNER], ['root_ca']);
+        $ca = $this->makeTestCert('ca', [CertificateFlag::CA, CertificateFlag::DOCUMENT_SIGNER], ['intermediate_ca']);
+        $signer = $this->makeTestCert('signer', [CertificateFlag::DOCUMENT_SIGNER], ['ca']);
+        
         $validChain = new Chain([
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[0])),
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[1])),
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[2])),
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[3])),
+            $signer,
+            $ca,
+            $intermediate_ca,
+            $root_ca,
         ]);
 
         $trustStore = new TrustStore([
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[3])),
+            $root_ca,
         ]);
 
         $result = Validator::validateChain($validChain, $trustStore);
@@ -174,15 +230,20 @@ class ValidatorTest extends TestCase
     {
         // Create chain where certificates exist but one doesn't sign the other
         // This would happen if we had certificates that aren't properly linked
+        $root_ca = $this->makeTestCert('root_ca', [CertificateFlag::ROOT_CA, CertificateFlag::DOCUMENT_SIGNER], ['root_ca']);
+        $intermediate_ca = $this->makeTestCert('intermediate_ca', [CertificateFlag::INTERMEDIATE_CA, CertificateFlag::DOCUMENT_SIGNER], ['root_ca']);
+        $ca = $this->makeTestCert('ca', [CertificateFlag::CA, CertificateFlag::DOCUMENT_SIGNER], ['intermediate_ca']);
+        $signer = $this->makeTestCert('signer', [CertificateFlag::DOCUMENT_SIGNER], ['ca']);
+        
         $chainWithMissingLink = new Chain([
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[0])), // Signer
+            $signer, // Signer
             // Skip the CA that should sign the signer certificate
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[2])), // Intermediate CA
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[3])), // Root CA
+            $intermediate_ca, // Intermediate CA
+            $root_ca, // Root CA
         ]);
 
         $trustStore = new TrustStore([
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[3])),
+            $root_ca,
         ]);
 
         $result = Validator::validateChain($chainWithMissingLink, $trustStore);
@@ -199,9 +260,14 @@ class ValidatorTest extends TestCase
     public function testValidateWithMultipleValidationErrors()
     {
         // Test comprehensive error collection
+        $root_ca = $this->makeTestCert('root_ca', [CertificateFlag::ROOT_CA, CertificateFlag::DOCUMENT_SIGNER], ['root_ca']);
+        $intermediate_ca = $this->makeTestCert('intermediate_ca', [CertificateFlag::INTERMEDIATE_CA, CertificateFlag::DOCUMENT_SIGNER], ['root_ca']);
+        $ca = $this->makeTestCert('ca', [CertificateFlag::CA, CertificateFlag::DOCUMENT_SIGNER], ['intermediate_ca']);
+        $signer = $this->makeTestCert('signer', [CertificateFlag::DOCUMENT_SIGNER], ['ca']);
+        
         $invalidChain = new Chain([
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[0])),
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[2])), // Skip middle cert
+            $signer,
+            $intermediate_ca, // Skip middle cert
         ]);
 
         $wrongTrustStore = new TrustStore([]); // Empty trust store
@@ -223,9 +289,11 @@ class ValidatorTest extends TestCase
 
     public function testValidateErrorMessageFormatting()
     {
+        $root_ca = $this->makeTestCert('root_ca', [CertificateFlag::ROOT_CA, CertificateFlag::DOCUMENT_SIGNER], ['root_ca']);
+        
         $emptyChain = new Chain([]);
         $trustStore = new TrustStore([
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[3])),
+            $root_ca,
         ]);
 
         $result = Validator::validateChain($emptyChain, $trustStore);
@@ -246,15 +314,20 @@ class ValidatorTest extends TestCase
     {
         // Test scenario that could generate multiple paths (though unlikely with current test data)
         // This test verifies the warning system works correctly
+        $root_ca = $this->makeTestCert('root_ca', [CertificateFlag::ROOT_CA, CertificateFlag::DOCUMENT_SIGNER], ['root_ca']);
+        $intermediate_ca = $this->makeTestCert('intermediate_ca', [CertificateFlag::INTERMEDIATE_CA, CertificateFlag::DOCUMENT_SIGNER], ['root_ca']);
+        $ca = $this->makeTestCert('ca', [CertificateFlag::CA, CertificateFlag::DOCUMENT_SIGNER], ['intermediate_ca']);
+        $signer = $this->makeTestCert('signer', [CertificateFlag::DOCUMENT_SIGNER], ['ca']);
+        
         $chain = new Chain([
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[0])),
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[1])),
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[2])),
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[3])),
+            $signer,
+            $ca,
+            $intermediate_ca,
+            $root_ca,
         ]);
 
         $trustStore = new TrustStore([
-            Certificate::fromBinary(BinaryString::fromBase64(self::EXAMPLE_CERTS[3])),
+            $root_ca,
         ]);
 
         $result = Validator::validateChain($chain, $trustStore);
